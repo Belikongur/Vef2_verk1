455710d23abab7b4570c00b4111b95c1ff374c5e
Abel Haukur <abelcat9@hotmail.com>
2022-01-23 20:47:26 +0000

    build script

--- src/build.js
+++ src/build.js
@@ -1,4 +1,4 @@
-import { readFile, readdir } from 'fs/promises';
+import { readFile, readdir, writeFile } from 'fs/promises';
 
 
 const DATA_DIR = './data';
@@ -7,6 +7,35 @@ async function main() {
     const files = await readdir(DATA_DIR);
 
     console.log("Files : " + files);
+    // Búa til Grunnsíðu index.html
+    const filename = 'index.html';
+    const indexSite = `<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width,initial-scale=1">
+        <title>${filename}</title>
+        <!-- Stylesheets -->
+        <link rel="stylesheet" href="styles.css" type="text/css">
+        <link rel="stylesheet" href="grid-overlay.css" type="text/css">
+    </head>
+    <body>
+    <h1>Gagnavinnsla</h1>
+    </body>
+</html>
+`;
+    try {
+        const result = await writeFile(`./dist/${filename}`, indexSite, { flag: 'w' });
+    }catch(e) {
+        console.error(`Failed to create ${index.html}`, e);
+    }
+    // Búa til linka á hvert einasta dataset
+
+    // Sannreyna gögnin og lagfæra
+
+    // Búa til datatable með gögnum úr data möppu
+    
+    
 
 }
 
